271b449987eb309c3dab780998e1d9faa86ea58e
moxiaoyuan <hqq1@admindeiMac-32.local>
2016-03-07 21:19:49 +0800

    bianliang
    变量常量

---
+++ Xcode_hqq.playground/Contents.swift
@@ -0,0 +1,41 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+//: Single line comment /*:
+/*:
+/*:
+A multiple line comment
+A multiple line comment
+A multiple line comment
+*/
+
+/*:
+# Title level 1
+## Title level 2
+### Title level 3
+#### Title level 4
+*/
+
+//: A *bold* or **Italic** font.
+
+/*:
++ unordered list item 1
++ unordered list item 2
++ unordered list item 3
+*/
+
+/*:
+1. ordered list item 1
+2. ordered list item 2
+3. ordered list item 3
+*/
+
+/*: This line will not be rendered.
+
+The above line
+
+----
+
+The bottom line
+*/
